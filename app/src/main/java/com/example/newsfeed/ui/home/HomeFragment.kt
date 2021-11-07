f89b4fa82f4ee8703a8622875e7658cd733b2526
SklRV <92019515+SklRV@users.noreply.github.com>
2021-11-08 01:39:19 +0300

    Приложение отображает список лучших фильмов (сайт https://www.themoviedb.org/ ) Используются: Bottom navbar, RecyclerView, Retrofit.

--- app/src/main/java/com/example/newsfeed/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/newsfeed/ui/home/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.example.newsfeed.ui.home
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,17 +9,19 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.newsfeed.MyTodayNewsRecyclerViewAdapter
-import com.example.newsfeed.R
-import com.example.newsfeed.placeholder.PlaceholderContent
+import com.example.newsfeed.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import com.example.newsfeed.retrofit_files.MovieApiClient
+import com.example.newsfeed.retrofit_files.MoviesAdapter
+import com.example.newsfeed.retrofit_files.MoviesResponse
 
 class HomeFragment : Fragment() {
 
     private var columnCount = 1
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         arguments?.let {
             columnCount = it.getInt(ARG_COLUMN_COUNT)
         }
@@ -29,24 +32,32 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_home, container, false)
+        val call = MovieApiClient.apiClient.getTopRatedMovies(API_KEY, "ru")
 
-        // Set the adapter
-        if (view is RecyclerView) {
-            with(view) {
-                layoutManager = when {
-                    columnCount <= 1 -> LinearLayoutManager(context)
-                    else -> GridLayoutManager(context, columnCount)
+        call.enqueue(object : Callback<MoviesResponse> {
+            override fun onResponse(
+                call: Call<MoviesResponse>,
+                response: Response<MoviesResponse>) {
+                val movies = response.body()!!.results
+                if (view is RecyclerView) {
+                    with(view) {
+                        layoutManager = when {
+                            columnCount <= 1 -> LinearLayoutManager(context)
+                            else -> GridLayoutManager(context, columnCount)
+                        }
+                        adapter = MoviesAdapter(movies, R.layout.list_item_movie)
+                    }
                 }
-                adapter = MyTodayNewsRecyclerViewAdapter(PlaceholderContent.ITEMS)
             }
-        }
+            override fun onFailure(call: Call<MoviesResponse>, t: Throwable) {
+                Log.e(TAG, t.toString())
+            }
+        })
         return view
     }
 
     companion object {
-
         const val ARG_COLUMN_COUNT = "column-count"
-
         @JvmStatic
         fun newInstance(columnCount: Int) =
             HomeFragment().apply {
@@ -54,5 +65,7 @@ class HomeFragment : Fragment() {
                     putInt(ARG_COLUMN_COUNT, columnCount)
                 }
             }
+        private val TAG = HomeFragment::class.java.simpleName
+        private val API_KEY = "8b4e4b775cb7c2606dc91c3429646ef0"
     }
 }
